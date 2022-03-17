41dbfd7d8a4cfe6929d1e93a4620c20855277c7a
Marko Mikulicic <mmikulicic@gmail.com>
2022-03-17 02:15:12 +0100

    Use relative paths

--- cmd/ursonnet/main.go
+++ cmd/ursonnet/main.go
@@ -13,7 +13,7 @@ type Context struct {
 }
 
 type CLI struct {
-	Path      string `arg:"" type:"path"`
+	Path      string `arg:""`
 	FieldPath string `arg:"" default:"$" help:"jsonnet field path, example, $.a.b"`
 	Debug     bool   `short:"d"`
 }
